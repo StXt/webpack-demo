00c7fb0bae2034b162e1eae09b117c5b2df40311
Stanislav Domanskyi <Stanislav_Domanskyi@epam.com>
2019-07-09 19:38:39 +0300

    output management

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.jpg';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+    const btn = document.createElement('button');
 
-    const myIcon = new Image();
-        myIcon.src = Icon;
-        element.appendChild(myIcon);
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
 
     return element;
 }
