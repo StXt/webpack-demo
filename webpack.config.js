4d1838baf2656ed37421a16b3120b35f214c39d2
StXt <stas.domanskiy@gmail.com>
2019-07-09 23:36:51 +0300

    edited structure

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: {
         app: './src/index.js',
-        print: './src/print.js'
     },
-    plugins: [
-            new HtmlWebpackPlugin({
-            title: 'Output Management'
-        })
-    ],
     output: {
-        filename: '[name].bundle.js',
+        filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
     }
 };
